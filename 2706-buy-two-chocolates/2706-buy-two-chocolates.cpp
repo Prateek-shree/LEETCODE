7d94857e95eb2b608b5ed39ac003e9523dbcb4a1
Prateek Shree <prateekshree2005@gmail.com>
2026-09-01 21:59:00 +0530

    Time: 0 ms (100%), Space: 52.1 MB (63.83%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        int first = INT_MAX;
+        int second = INT_MAX;
+
+        for (int price : prices) {
+
+            if (price < first) {
+                second = first;
+                first = price;
+            } else if (price < second) {
+                second = price;
+            }
+        }
+
+        int cost = first + second;
+
+        if (cost <= money)
+            return money - cost;
+
+        return money;
+    }
+};
